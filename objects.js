2589cb2d917c6c15cba4172edb2be833b5c55a1c
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-16 15:36:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist(playlist,artist,song) {
   playlist[artist] = song
   return playlist
 }
+function removeFromPlaylist(playlist,artist) {
+  delete playlist[artist]
+  return playlist
+}
